6f35de8f9390834708eb20d6bc4e857756e91ccf
Thrisha1 <thrishakannan25@gmail.com>
2025-03-24 23:02:07 +0530

    merging

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -82,7 +82,7 @@ define(['./workbox-54d0af47'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.ol5iuuj0vho"
+    "revision": "0.3i4h1h2j9u"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
